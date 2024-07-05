a2587eff946d64f30c4712ded81f90a30cd98919
DeviSriMonica <140843076+DeviSriMonica@users.noreply.github.com>
2024-07-05 16:01:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ============= typing animation ==============*/
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "web Developer", "Graphics Designer", "Freelancer"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true,
+})
+
+
+
+
